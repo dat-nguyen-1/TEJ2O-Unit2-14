7f1cba86154652193ab36d9c8a274860196d55da
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-04-15 13:28:34 -0400

    add happy face

--- main.ts
+++ main.ts
@@ -5,75 +5,97 @@
  * This program will move a pixel around the perimeter.
 */
 
-// create sprite
-let sprite = game.createSprite(0, 0)
+// initialize display
+basic.showIcon(IconNames.Happy)
+
+// initialize counters
+let xCounter = 0
+let yCounter = 0
 
 // handle button A press
 input.onButtonPressed(Button.A, function() {
+    // create sprite
+    let sprite = game.createSprite(0, 0)
+
     // reset counters
-    let xCounter = 0
-    let yCounter = 0
+    xCounter = 0
+    yCounter = 0
 
     // move right
     while (xCounter <= 4) {
+        basic.pause(100)
         sprite.change(LedSpriteProperty.X, 1)
         xCounter += 1
-        basic.pause(100)
     }
 
     // move down
     while (yCounter <= 4) {
+        basic.pause(100)
         sprite.change(LedSpriteProperty.Y, 1)
         yCounter += 1
-        basic.pause(100)
     }
 
     // move left
     while (xCounter >= 0) {
+        basic.pause(100)
         sprite.change(LedSpriteProperty.X, -1)
         xCounter -= 1
-        basic.pause(100)
     }
 
     // move up
     while (yCounter >= 0) {
+        basic.pause(100)
         sprite.change(LedSpriteProperty.Y, -1)
         yCounter -= 1
-        basic.pause(100)
     }
+
+    // delete sprite
+    sprite.delete()
+
+    // reset display
+    basic.showIcon(IconNames.Happy)
 })
 
 // handle button B press
 input.onButtonPressed(Button.B, function () {
+    // create sprite
+    let sprite = game.createSprite(0, 0)
+
     // reset counters
-    let xCounter = 0
-    let yCounter = 0
+    xCounter = 0
+    yCounter = 0
 
     // move down
     while (yCounter <= 4) {
+        basic.pause(100)
         sprite.change(LedSpriteProperty.Y, 1)
         yCounter += 1
-        basic.pause(100)
     }
 
     // move right
     while (xCounter <= 4) {
+        basic.pause(100)
         sprite.change(LedSpriteProperty.X, 1)
         xCounter += 1
-        basic.pause(100)
     }
 
     // move up
     while (yCounter >= 0) {
+        basic.pause(100)
         sprite.change(LedSpriteProperty.Y, -1)
         yCounter -= 1
-        basic.pause(100)
     }
 
     // move left
     while (xCounter >= 0) {
+        basic.pause(100)
         sprite.change(LedSpriteProperty.X, -1)
         xCounter -= 1
-        basic.pause(100)
     }
+
+    // delete sprite
+    sprite.delete()
+
+    // reset display
+    basic.showIcon(IconNames.Happy)
 })
