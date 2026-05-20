60f06e24e4ad20ca6e10eace1e773306cb291d4d
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-05-20 13:27:30 -0400

    Update main.py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,4 +6,44 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
-display.scroll("Hello, World!")
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.was_pressed():
+        
+        display.clear()
+        # reset display
+        # reset counters
+        x_counter = 0
+        y_counter = 0
+        
+        # move right
+        while x_counter < 4:
+            display.set_pixel(x_counter, y_counter, 9)
+            x_counter += 1
+            sleep(100)
+            display.clear()
+
+        # move down
+        while y_counter < 4:
+            display.set_pixel(x_counter, y_counter, 9)
+            y_counter += 1
+            sleep(100)
+            display.clear()
+
+        # move left
+        while x_counter > 0:
+            display.set_pixel(x_counter, y_counter, 9)
+            x_counter -= 1
+            sleep(100)
+            display.clear()
+
+        # move up
+        while y_counter >= 0:
+            display.set_pixel(x_counter, y_counter, 9)
+            y_counter -= 1
+            sleep(100)
+            display.clear()
+
+        display.show(Image.HAPPY)
+        
