7b6065afce2fc9627ee793cb4f1c781e96d72b4d
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-05-21 13:24:29 -0400

    Implement display reset and pixel movement with button_b
    Added functionality to reset display and counters with button_b, moving a pixel in all directions.

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -10,9 +10,9 @@ display.show(Image.HAPPY)
 
 while True:
     if button_a.was_pressed():
-        
-        display.clear()
         # reset display
+        display.clear()
+
         # reset counters
         x_counter = 0
         y_counter = 0
@@ -42,6 +42,44 @@ while True:
         while y_counter >= 0:
             display.set_pixel(x_counter, y_counter, 9)
             y_counter -= 1
+            sleep(100)
+            display.clear()
+
+        display.show(Image.HAPPY)
+
+    if button_b.was_pressed():
+        # reset display
+        display.clear()
+
+        # reset counters
+        x_counter = 0
+        y_counter = 0
+
+        # move down
+        while y_counter < 4:
+            display.set_pixel(x_counter, y_counter, 9)
+            y_counter += 1
+            sleep(100)
+            display.clear()
+        
+        # move right
+        while x_counter < 4:
+            display.set_pixel(x_counter, y_counter, 9)
+            x_counter += 1
+            sleep(100)
+            display.clear()
+
+        # move up
+        while y_counter > 0:
+            display.set_pixel(x_counter, y_counter, 9)
+            y_counter -= 1
+            sleep(100)
+            display.clear()
+
+        # move left
+        while x_counter >= 0:
+            display.set_pixel(x_counter, y_counter, 9)
+            x_counter -= 1
             sleep(100)
             display.clear()
 
